45a4e8290de37c6fa6b0a9b4846b456aa09eb107
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-13 21:21:50 -0700

    Complete functions.js

--- functions.js
+++ functions.js
@@ -1,14 +1,31 @@
-'use strict';
+"use strict";
 
 // 1. isHometown
-
-// Define your function here
+function isHometown(town) {
+  return town === "San Francisco";
+}
 
 // 2. getFullName
-
-// Define your function here
+function getFullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
 
 // 3. calculateTotal
+function calculateTotal(basePrice, state, tax = 0.05) {
+  let subtotal = basePrice * (1 + tax);
+  let fee = 0;
 
-// Define your function here
-   
+  switch (state) {
+    case "CA":
+      fee = 0.03 * subtotal;
+      break;
+    case "PA":
+      fee = 2;
+      break;
+    case "MA":
+      basePrice <= 100 ? (fee = 1) : (fee = 3);
+      break;
+  }
+
+  return subtotal + fee;
+}
