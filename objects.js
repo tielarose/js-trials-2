817fdad17c2bd5dd1013ed6962327fe05444536a
Elizabeth Joyce <ekj28@cornell.edu>
2023-04-13 21:02:46 -0700

    Write getmelonsatprice and countwords functions

--- objects.js
+++ objects.js
@@ -2,10 +2,35 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {}
+
+  for (let word of phrase.split(" ")) { 
+    if (word in wordCounts) {
+      wordCounts[word] += 1;
+    } else { 
+      wordCounts[word] = 1;
+    }
+  }
+
+  return wordCounts
 }
+
+// const myPhrase = 'hello hi hola hi yo hola'
+// console.log(countWords(myPhrase))
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  let melonPrices = {
+        2.50: ['Cantaloupe', 'Honeydew'],
+        2.95: ['Watermelon'],
+        3.25: ['Musk', 'Crenshaw', 'Greenmelon'],
+        14.25: ['Christmas']
+    }
+
+  return melonPrices[price].sort()
+  
 }
+   
+
+console.log(getMelonsAtPrice(3.25))
+
