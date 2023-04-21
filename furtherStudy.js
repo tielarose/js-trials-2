ceecbf7943f4522f9a9a42fdbc160699a30e4089
Tiela Black-Law <tiela.rose@gmail.com>
2023-04-20 19:35:15 -0700

    Complete wordsInCommon function

--- furtherStudy.js
+++ furtherStudy.js
@@ -1,10 +1,20 @@
-'use strict';
+"use strict";
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const words1Set = new Set(words1);
+  const words2Set = new Set(words2);
+
+  const result = new Set();
+
+  for (const word of words1Set) {
+    if (words2Set.has(word)) {
+      result.add(word);
+    }
+  }
+
+  return result;
 }
 
 function kidsGame(names) {
   // Replace this with your code
 }
-   
